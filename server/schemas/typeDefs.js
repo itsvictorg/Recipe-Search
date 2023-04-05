05fa3087ddd5f1b8e71d4bf893d20ebc2bbc2db3
itsvictorg <itsvictorg@gmail.com>
2023-04-04 20:19:05 -0500

    initial commit. starting from book search engine template

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    image: String
+    title: String!
+    link: String
+  }
+
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    title: String!
+    description: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
