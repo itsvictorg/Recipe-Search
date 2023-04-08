1681e4b21f316e8f69667504941d5d9810cccef2
zMag33z <mag33.cdh@gmail.com>
2023-04-08 14:16:15 -0500

    styling files added to folder

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,13 +2,13 @@
 
 import React from 'react';
 
-
+import '../styles/Contact.css';
 
 
 const Contact = () => {
   return (
     <>
-    <section>
+    <section className='page-cont'>
         <h2>Contact Us Anytime!</h2>
       <div>
         <p>Not sure what to put in here as of yet.</p>
